25b45d17209b78fe1d25b97f7cfbfcdb2e42d59f
Uhan7 <luckyfellowfella@gmail.com>
2026-03-04 18:00:53 +0800

    woohoo insertion works

--- Assets/Scripts/General/DropZone.cs
+++ Assets/Scripts/General/DropZone.cs
@@ -1,15 +1,31 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class DropZone : MonoBehaviour, IDropHandler
 {
     // Variables ---------------------------------------------------------------
+    [SerializeField] private bool allowInsertion;
 
     // Main Functions ----------------------------------------------------------
     public void OnDrop(PointerEventData eventData)
     {
-        eventData.pointerDrag.transform.SetParent(gameObject.transform);
-    }
+        GameObject draggedObject = eventData.pointerDrag.gameObject;
+        RectTransform rectTransform = transform as RectTransform;
+        Vector2 localMousePosition;
+        int newIndex = 0;
 
-    // Helper Functions --------------------------------------------------------
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, eventData.position, eventData.pressEventCamera, out localMousePosition);
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            RectTransform child = transform.GetChild(i) as RectTransform;
+            if (child.gameObject == draggedObject) continue;
+
+            if (localMousePosition.x > child.localPosition.x) newIndex = i + 1;
+        }
+
+        draggedObject.transform.SetParent(transform);
+        if (allowInsertion) draggedObject.transform.SetSiblingIndex(newIndex);
+    }
 }
