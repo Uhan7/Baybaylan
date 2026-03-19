e0d83ff84a714a78576f5eaf84ba86683261a825
Uhan7 <luckyfellowfella@gmail.com>
2026-03-19 11:39:01 +0800

    connected some sfx and did cleanup
    still gotta do bg changes

---
+++ Assets/Scripts/Managers/BackgroundsManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class BackgroundsManager : MonoBehaviour
+{
+    // Variables ---------------------------------------------------------------
+    [Header("Background Objects")]
+    [SerializeField] private GameObject purifiedBackground;
+    [SerializeField] private GameObject corruptedBackground;
+
+    // Main Functions ----------------------------------------------------------
+
+    // Helper Functions --------------------------------------------------------
+}
