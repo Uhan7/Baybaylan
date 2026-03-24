e4eaa9c5bab7c146906401c6c98217ffcd21ebc7
Uhan7 <luckyfellowfella@gmail.com>
2026-03-24 10:28:40 +0800

    started on dialogue ui moveing shitheiht

---
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    // Variables ---------------------------------------------------------------
+    [Header("Dialogue Reference")]
+	[SerializeField] private Dialogue dialogue;
+    [SerializeField] private DialogueTrigger nextDialogue;
+
+    [Header("Properties")]
+    [SerializeField] private bool activateOnEnable;
+    [SerializeField] private bool isRepeatable;
+
+    [Header("Flags")]
+    [ReadOnly, SerializeField] private bool alreadyTriggered;
+    [HideInInspector] private bool waitingForEnd;
+
+    // Main Functions ----------------------------------------------------------
+    private void OnEnable()
+    {
+        if (activateOnEnable) TriggerDialogue();
+    }
+
+    private void Update()
+    {
+        if (waitingForEnd && DialogueManager.endConvo) Destroy(gameObject, .1f);
+        if (alreadyTriggered && nextDialogue != null && DialogueManager.endConvo)
+        {
+            nextDialogue.gameObject.SetActive(true);
+        }
+    }
+
+    // Helper Functions --------------------------------------------------------
+    public void TriggerDialogue()
+	{
+        if (alreadyTriggered && !isRepeatable) return;
+
+        StartCoroutine(FindFirstObjectByType<DialogueManager>().StartDialogue(dialogue));
+        waitingForEnd = true;
+
+        alreadyTriggered = true;
+    }
+}
